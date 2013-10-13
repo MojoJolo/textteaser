4a9eb77d811782df4e1cefc56a596d399d868592
Felipe Hummel <felipehummel@gmail.com>
2013-10-13 16:48:31 -0400

    Added an option to GuiceModule to use DummyKeywordService instead of the Mongo one

--- src/main/scala/com/textteaser/summarizer/GuiceModule.scala
+++ src/main/scala/com/textteaser/summarizer/GuiceModule.scala
@@ -8,12 +8,15 @@ import java.io.FileInputStream
 import com.mongodb._
 import org.slf4j.LoggerFactory
 
-class GuiceModule(config: Config) extends AbstractModule with ScalaModule {
+class GuiceModule(config: Config, dummyKeywordService: Boolean = false) extends AbstractModule with ScalaModule {
   def configure {
     bind[Config].in[Singleton]
     bind[Parser].in[Singleton]
     bind[Summarizer].in[Singleton]
-    bind[KeywordService].in[Singleton]
+    if (dummyKeywordService)
+      bind[KeywordService].to[DummyKeywordService].in[Singleton]
+    else
+      bind[KeywordService].to[MongoKeywordService].in[Singleton]
   }
 
   @Provides
