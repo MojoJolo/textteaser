b1aac63bd01f7ba15b2521329c0f60281cf26d79
Felipe Hummel <felipehummel@gmail.com>
2013-10-13 16:31:47 -0400

    Created DummyKeywordService

--- src/main/scala/com/textteaser/summarizer/KeywordService.scala
+++ src/main/scala/com/textteaser/summarizer/KeywordService.scala
@@ -7,3 +7,11 @@ trait KeywordService {
   def getCategoryScore(word: String, cat: String): Long
   def add(word: String, count: Long, summaryId: String, blog: String, cat: String): Unit
 }
+
+class DummyKeywordService extends KeywordService {
+  def getBlogCount(blog: String): Long = 1
+  def getCategoryCount(cat: String): Long = 1
+  def getBlogScore(word: String, blog: String): Long = 1
+  def getCategoryScore(word: String, cat: String): Long = 1
+  def add(word: String, count: Long, summaryId: String, blog: String, cat: String) {}
+}
